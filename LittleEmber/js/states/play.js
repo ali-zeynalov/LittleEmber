ca3eaf92b2248357950db84c811ee7ea663b712e
Ali Zeynalov <ali@zeynalov.com>
2019-05-09 16:56:11 -0700

    * Replaced individual sprites with a texture atlas * Added collision detection * Fixed some minor things

--- LittleEmber/js/states/play.js
+++ LittleEmber/js/states/play.js
@@ -36,67 +36,66 @@ Play.prototype = {
         this.obstacles = game.add.group();
 
         // Every time spawns obstacles
-        game.time.events.loop(Phaser.Timer.SECOND * 2, this.createObstacle, this);this.previousObstacleIndex = 0;
+        game.time.events.loop(Phaser.Timer.SECOND * 2, this.createObstacle, this);
+        this.previousObstacleIndex = 0;
         this.obstacles = game.add.group();
         console.log("State: Play");
 
         // make player character
-        this.player = game.add.sprite(game.world.width/2, game.world.height - 50, "littleEmber"); // for atlas: (x, y, nameOfAtlas, assetNameInAtlas)
-        // player.animations.add('fly', ["kiwi_0000", "kiwi_0001", "kiwi_0002", "kiwi_0003"], 4, true);
-        // uncomment if we need to resize
-		// player.scale.x = 0.2;
-		// player.scale.y = 0.2;
-		this.player.scale.setTo(0.5, 0.5);
-		this.player.anchor.set(0.5); // center the mass of player character
-		// player physics
-		game.physics.enable(this.player, Phaser.Physics.ARCADE);
-		this.player.body.maxVelocity.set(this.PLAYER_MAX_VELOCITY);
-		this.player.body.drag.set(700);
-		this.player.body.collideWorldBounds = true;
-        // player.body.immovable = true;
-        // player.body.gravity.y = 300;
+        this.player = game.add.sprite(game.world.width / 2, game.world.height - 50, "atlas", "littleEmber"); // for atlas: (x, y, nameOfAtlas, assetNameInAtlas)
+        this.player.scale.setTo(0.5, 0.5);
+        this.player.anchor.set(0.5); // center the mass of player character
+        // player physics
+        game.physics.enable(this.player, Phaser.Physics.ARCADE);
+        this.player.body.maxVelocity.set(this.PLAYER_MAX_VELOCITY);
+        this.player.body.drag.set(700);
+        this.player.body.collideWorldBounds = true;
     },
     update: function () {
         this.grassBg.tilePosition.y += this.SCROLLING_SPEED_GRASS;
         // allow the player to exit game to GameOver state by pressing Q
-        if(game.input.keyboard.isDown(Phaser.Keyboard.Q)) {
+        if (game.input.keyboard.isDown(Phaser.Keyboard.Q)) {
             game.state.add("GameOver", GameOver);
-			game.state.start("GameOver");
+            game.state.start("GameOver");
         }
 
         // add player input checks
         // since they're not in else ifs, we should be able to get combinatorial movement
         // these combo movements are not as fast as they should technically be, so might add
         // euclidian combinatorial directionns instead
-        if(game.input.keyboard.isDown(Phaser.Keyboard.W)) {
+        if (game.input.keyboard.isDown(Phaser.Keyboard.W)) {
             this.player.body.velocity.y -= 75;
         }
         if (game.input.keyboard.isDown(Phaser.Keyboard.S)) {
-			this.player.body.velocity.y += 75;
+            this.player.body.velocity.y += 75;
         }
-        if(game.input.keyboard.isDown(Phaser.Keyboard.A)) {
+        if (game.input.keyboard.isDown(Phaser.Keyboard.A)) {
             this.player.body.velocity.x -= 75;
         }
-        if(game.input.keyboard.isDown(Phaser.Keyboard.D)) {
+        if (game.input.keyboard.isDown(Phaser.Keyboard.D)) {
             this.player.body.velocity.x += 75;
         }
         // if stuff isn't being held down to move, slow to a stop
-        if(!game.input.keyboard.isDown(Phaser.Keyboard.W) && !game.input.keyboard.isDown(Phaser.Keyboard.S)) {
-            if(this.player.body.velocity.y > 300) { // if we were going down
-            this.player.body.velocity.y -= 15;
+        if (!game.input.keyboard.isDown(Phaser.Keyboard.W) && !game.input.keyboard.isDown(Phaser.Keyboard.S)) {
+            if (this.player.body.velocity.y > 300) { // if we were going down
+                this.player.body.velocity.y -= 15;
             }
-            else if(this.player.body.velocity.y < 300) { // if we were going up
+            else if (this.player.body.velocity.y < 300) { // if we were going up
                 this.player.body.velocity.y += 30;
             }
         }
-        if(!game.input.keyboard.isDown(Phaser.Keyboard.A) && !game.input.keyboard.isDown(Phaser.Keyboard.D)) {
-            if(this.player.body.velocity.x > 0) { // if we were going down
+        if (!game.input.keyboard.isDown(Phaser.Keyboard.A) && !game.input.keyboard.isDown(Phaser.Keyboard.D)) {
+            if (this.player.body.velocity.x > 0) { // if we were going down
                 this.player.body.velocity.x -= 15;
             }
-            else if(this.player.body.velocity.x < 0) { // if we were going up
+            else if (this.player.body.velocity.x < 0) { // if we were going up
                 this.player.body.velocity.x += 15;
             }
         }
+
+        // Player overlapping objects
+        game.physics.arcade.overlap(this.player, this.obstacles, this.playerOverlap, null, this);
+
     },
     createObstacle: function () {
         // Creating the obstacle
@@ -114,11 +113,28 @@ Play.prototype = {
         var maxVelocity = this.OBSTACLE_MAX_VELOCITY;
 
         this.obstacle = new Obstacle(game, x, y, direction, this.LEVELS[this.level].obstacles[obstacleIndex], xVelocity, yVelocity, maxVelocity);
+        // Add the object to the game
         game.add.existing(this.obstacle);
-
-        /***
-         * TODO: adding it to the group which later on used for collision detection
-         */
         this.obstacles.add(this.obstacle);
+    },
+    playerOverlap: function (player, obstacle) {
+        if (!obstacle.burning) {
+            obstacle.burning = true;
+            obstacle.animations.play("burning", true);
+            /***
+             * TODO: Add sound change here as well
+             */
+            game.time.events.add(Phaser.Timer.SECOND, this.switchToAshe, this, obstacle);
+            /***
+             * TODO: Add score and burn meter change here
+             */
+
+        }
+    },
+    switchToAshe: function (obstacle) {
+        /***
+         * TODO: Add sounds change here
+         */
+        obstacle.animations.play("ashe", true);
     }
 };
